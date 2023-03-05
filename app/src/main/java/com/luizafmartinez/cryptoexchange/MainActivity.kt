e37e06f929fa87b2dbb283c3cc9a7d3e051d249a
alxmart <alxmartinez@gmail.com>
2023-03-05 12:20:41 -0300

    Added code, defining variables.

--- app/src/main/java/com/luizafmartinez/cryptoexchange/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/cryptoexchange/MainActivity.kt
@@ -2,10 +2,28 @@ package com.luizafmartinez.cryptoexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRecuperar: Button
+    lateinit var textBitcoin: TextView
+    lateinit var textEthereum: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRecuperar = findViewById(R.id.btn_recuperar)
+        textBitcoin = findViewById(R.id.tv_bitcoin)
+        textEthereum = findViewById(R.id.tv_ethereum)
+
+        btnRecuperar.setOnClickListener {
+
+        }
+
+
+
     }
 }
